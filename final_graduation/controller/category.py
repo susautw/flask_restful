855d3a0edd850f650163501a02ded679e57a7dd8
susautw <susautw@gmail.com>
2020-05-21 02:04:43 +0800

    Modify category.py - Add CategoryController - Add CategoryItemController

--- final_graduation/controller/category.py
+++ final_graduation/controller/category.py
@@ -1,5 +1,20 @@
+from bson import ObjectId
 from flask_restful import Resource
+from .. import models
+from ..kernal import Response
 
 
 class CategoryController(Resource):
-    pass
+    def get(self):
+        res = Response()
+        with res:
+            res.result = list(models.Category.get_all_categories())
+        return res
+
+
+class CategoryItemController(Resource):
+    def get(self, category_id: str):
+        res = Response()
+        with res:
+            res.result = models.Category.get_by_id(ObjectId(category_id))
+        return res
